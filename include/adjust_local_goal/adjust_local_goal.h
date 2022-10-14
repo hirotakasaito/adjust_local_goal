0d632e2c0248f9bfdc3f2225dabe87cc7de725cd
hirotakasaito <tirohiro20208@gmail.com>
2022-10-14 15:27:00 +0900

    fix

--- include/adjust_local_goal/adjust_local_goal.h
+++ include/adjust_local_goal/adjust_local_goal.h
@@ -3,6 +3,17 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <stdlib.h>
+
+struct Map_info
+{
+    float dis;
+    int dr;
+    int dc;
+    int i;
+    int j;
+    float map_cost;
+};
 
 class AdjustLocalGoal
 {
@@ -13,10 +24,11 @@ public:
 private:
     void local_goal_callback(const geometry_msgs::PoseStampedConstPtr &);
     void local_map_callback(const nav_msgs::OccupancyGrid::ConstPtr &);
-    void calc_safe_zone(int i, int j);
     void adjust_local_goal(void);
 
     int HZ;
+    float MAP_COST_GAIN;
+    float DISTANCE_GAIN;
     int local_goal_index_x = 0;
     int local_goal_index_y = 0;
     int adjust_local_goal_index_x;
@@ -25,22 +37,45 @@ private:
     int row;
     int column;
     float resolution;
+    float cost;
     int divide;
     int max_divide_x;
     int max_divide_y;
     int divide_index_x;
     int divide_index_y;
+    int min_dr;
+    int min_dc;
+    int min_i;
+    int min_j;
+    int dis_min_dr;
+    int dis_min_dc;
+    int dis_min_i;
+    int dis_min_j;
 
+    int divide_resolution;
+    int map_cost;
+    int _map_cost;
+    int in;
+    int jn;
+    float dx;
+    float dy;
+    float dis;
+    float min_dis;
+    float dis_min_dis;
+    bool enable_change;
     bool local_goal_updated;
     bool local_map_updated;
     bool change_goal;
-    std::vector<std::vector<int>> map;
+    // std::vector<std::vector<std::vector<std::vector<int>>>> divide_map;
+    // std::vector<std::vector<std::vector<std::vector<int>>>> map;
+    std::vector<int> map_costs;
+    std::vector<Map_info> map_infos;
+    Map_info map_info;
 
     ros::Publisher local_goal_pub;
     ros::Subscriber local_goal_sub;
     ros::Subscriber local_map_sub;
 
-    ros::NodeHandle local_nh;
     ros::NodeHandle nh;
 
     nav_msgs::OccupancyGrid local_map;
--- src/adjust_local_goal.cpp
+++ src/adjust_local_goal.cpp
@@ -1,15 +1,19 @@
 #include "adjust_local_goal/adjust_local_goal.h"
 
 AdjustLocalGoal::AdjustLocalGoal(void)
-    :local_nh("~")
+    :nh("~")
 {
-    local_nh.param("HZ", HZ, {10});
-    local_nh.param("HZ", divide, {1});
+    nh.param("HZ", HZ, {50});
+    nh.param("DIVIDE", divide, {10});
+    nh.param("MAP_COST_GAIN", MAP_COST_GAIN, {1.0});
+    nh.param("DISTANCE_GAIN", DISTANCE_GAIN, {1.0});
 
     ROS_INFO("===param===");
 
     ROS_INFO_STREAM("HZ: " <<HZ);
-    ROS_INFO_STREAM("divide: " <<10);
+    ROS_INFO_STREAM("divide: " <<divide);
+    ROS_INFO_STREAM("MAP_COST_GAIN: " <<MAP_COST_GAIN);
+    ROS_INFO_STREAM("DISTANCE_GAIN: " <<DISTANCE_GAIN);
 
     local_goal_sub = nh.subscribe("/local_goal", 1, &AdjustLocalGoal::local_goal_callback, this);
     local_map_sub = nh.subscribe("/local_map/expand", 1, &AdjustLocalGoal::local_map_callback, this);
@@ -35,35 +39,91 @@ void AdjustLocalGoal::adjust_local_goal(void)
     column = local_map.info.height;
     row = local_map.info.width;;
     resolution = local_map.info.resolution;
-    map.resize(row, std::vector<int>(column));
+    divide_resolution = row / divide;
 
     local_goal_index_x = int(local_goal.pose.position.x / resolution + column/2);
     local_goal_index_y = int(-1 * local_goal.pose.position.y / resolution + row/2);
 
-    // ROS_INFO_STREAM("local_goal_index_x" <<local_goal_index_x);
-    // ROS_INFO_STREAM("local_goal_index_y" <<local_goal_index_y);
+    float min_cost = 5e5;
 
-    for(int i=0; i<row; i++)
-    {
-        for(int j=0; j<column; j++)
-        {
-            map[i][j] = local_map.data[j*row+i];
-        }
-    }
-
-    // ROS_INFO_STREAM(map[local_goal_index_x][local_goal_index_y]);
-    if(map[local_goal_index_x][local_goal_index_y] == 100)
+    if(local_map.data[local_goal_index_x*row + local_goal_index_y] == 100)
     {
        ROS_INFO_STREAM("change goal");
-       // ROS_INFO_STREAM(map[local_goal_index_x][local_goal_index_y]);
        change_goal = true;
-       calc_safe_zone(local_goal_index_x,local_goal_index_y);
+       // calc_safe_zone(local_goal_index_x,local_goal_index_y);
+
+        for(int dr=0; dr<divide; dr++)
+        {
+            for(int dc=0; dc<divide; dc++)
+            {
+                map_cost = 0;
+                min_dis = 5e5;
+                enable_change = false;
+                for(int i=0; i<divide_resolution; i++)
+                {
+                    for(int j=0; j<divide_resolution; j++)
+                    {
+                        if(local_map.data[(divide_resolution*dc+j)*row+(divide_resolution*dr+i)] == 0)
+                        {
+                            dx = ((divide_resolution*dc+j) - local_goal_index_x)*resolution;
+                            dy = ((divide_resolution*dr+i) - local_goal_index_y)*resolution;
+                            dis = std::sqrt(std::pow(dx,2.0) + std::pow(dy,2.0));
+
+                            if(dis < min_dis)
+                            {
+                                min_dis = dis;
+                                dis_min_dr = dr;
+                                dis_min_dc = dc;
+                                dis_min_i = i;
+                                dis_min_j = j;
+                            }
+
+                            enable_change = true;
+                        }
+
+                        _map_cost = local_map.data[(divide_resolution*dc+j)*row+(divide_resolution*dr+i)];
+                        if(_map_cost == -1) _map_cost = 100;
+                        map_cost += _map_cost;
+
+                    }
+                }
+                // if(!enable_change) break;
+                map_info.dr = dis_min_dr;
+                map_info.dc = dis_min_dc;
+                map_info.i = dis_min_i;
+                map_info.j = dis_min_j;
+                map_info.dis = min_dis;
+                map_info.map_cost = map_cost;
+                map_infos.push_back(map_info);
+
+            }
+        }
+
+        for(const auto& map_info : map_infos)
+        {
+
+            cost = MAP_COST_GAIN * map_info.map_cost + DISTANCE_GAIN * map_info.dis;
+
+            if(cost < min_cost)
+            {
+                min_cost = cost;
+                min_dr = map_info.dr;
+                min_dc = map_info.dc;
+                min_i = map_info.i;
+                min_j = map_info.j;
+            }
+        }
+        // ROS_INFO_STREAM(min_cost);
     }
 
     geometry_msgs::PoseStamped adjust_local_goal;
 
     if(change_goal)
     {
+        change_goal = false;
+        // ROS_INFO_STREAM(min_dc);
+        adjust_local_goal_index_x = divide_resolution * min_dc + min_j;
+        adjust_local_goal_index_y = divide_resolution * min_dr + min_i;
         adjust_local_goal.pose.position.x = (adjust_local_goal_index_x - column/2)*resolution;
         adjust_local_goal.pose.position.y = (adjust_local_goal_index_y - row/2)*resolution*(-1);
 
@@ -80,78 +140,7 @@ void AdjustLocalGoal::adjust_local_goal(void)
         adjust_local_goal = local_goal;
         local_goal_pub.publish(adjust_local_goal);
     }
-
-    change_goal = false;
 }
-
-void AdjustLocalGoal::calc_safe_zone(int i,int j)
-{
-    divide_index_x = i%divide;
-    divide_index_y = j%divide;
-    max_divide_x = row%divide;
-    max_divide_y = column%divide;
-    float min_dis = 1e5;
-    float dis = 0.0;
-    float dx = 0.0;
-    float dy = 0.0;
-
-    // for(int zi=divide*divide_index_x; zi<divide*divide_index_x+divide; zi++)
-    // {
-    //     for(int zj=divide*divide_index_y; zj<divide*divide_index_y+divide; zj++)
-    //     {
-    //         if(local_map.data[zi*row +zj] == 0)
-    //         {
-    //             cx = std::pow(zj*resolution,2.0);
-    //             cy = std::pow(zi*resolution,2.0);
-    //             dx = cx - local_goal.pose.position.x;
-    //             dy = cy - local_goal.pose.position.y;
-    //             dis = std::sqrt(std::pow(dx,2.0) + std::pow(dy,2.0));
-    //             if(min_dis > dis)
-    //             {
-    //                 min_dis = dis;
-    //                 local_goal_index_x = zj;
-    //                 local_goal_index_y = zi;
-    //             }
-    //         }
-    //     }
-    // }
-    float robot_r = 2.0;
-
-    for(int zi=0; zi<column; zi++)
-    {
-        for(int zj=0; zj<row; zj++)
-        {
-            if(map[zi][zj] == 0)
-            {
-                dx = (zi - local_goal_index_x)*resolution;
-                dy = (zj - local_goal_index_y)*resolution;
-                dis = std::sqrt(std::pow(dx,2.0) + std::pow(dy,2.0));
-                if(dis < robot_r)
-                {
-                    continue;
-                }
-
-                else if(min_dis > dis)
-                {
-                    min_dis = dis;
-                    adjust_local_goal_index_x = zi;
-                    adjust_local_goal_index_y = zj;
-
-                    ROS_INFO_STREAM(dis);
-                }
-                // ROS_INFO_STREAM("x" <<local_goal_index_x);
-                // ROS_INFO_STREAM("y" << local_goal_index_y);
-
-            }
-        }
-    }
-}
-
-// void AdjustLocalGoal::next_zone(divide_index_x, divide_index_y)
-// {
-//
-    // for()
-// }
 
 void AdjustLocalGoal::process(void)
 {
